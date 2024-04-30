32d450c5d18419b8b2eebd8403cde7d15959cf97
Asif <msasifiiuc@gmail.com>
2024-04-30 11:55:24 +0600

    all ok

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
           <h1 className="mb-5 text-4xl font-bold">Welcome to Employee Management!</h1>
           <div className="flex items-center justify-center">
             <Link to="/dashboard/departments">
-              <button className="btn bg-bg1 hover:bg-bgk px-4 py-1 rounded font-bold flex items-center gap-1 justify-center">DashBoard <BsFillArrowUpRightCircleFill /></button>
+              <button className="btn bg-sl1 border-sl1 hover:bg-sl1 px-4 py-1 rounded font-bold flex items-center gap-1 justify-center">DashBoard <BsFillArrowUpRightCircleFill className="text-xl"/></button>
             </Link>
           </div>
         </div>
